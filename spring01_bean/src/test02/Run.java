88839605fb57f4b4c0086ca19634c3a11809f9be
cxyxj98 <cxyxj98@gmail.com>
2021-10-16 23:49:55 +0900

    [spring01_bean]

---
+++ spring01_bean/src/test02/Run.java
@@ -0,0 +1,14 @@
+package test02;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext context = new ClassPathXmlApplicationContext("test02/applicationContext.xml");
+		System.out.println("오늘은 "+ context.getBean("dayInfo",Week.class).dayInfo()+" 입니다.");
+	}
+
+}
