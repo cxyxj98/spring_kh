bd140ac2fd34373827307bf72d332a2af6b7d450
cxyxj98 <cxyxj98@gmail.com>
2021-10-22 23:30:41 +0900

    10월 22일 이메일 회원가입 구현

--- spring06_toyProject/src/main/java/com/kh/spring/common/code/Config.java
+++ spring06_toyProject/src/main/java/com/kh/spring/common/code/Config.java
@@ -4,7 +4,7 @@ public enum Config {
 	
 	//DOMAIN("https://pclass.ga"),
 	
-	DOMAIN("https://localhost:9090"),
+	DOMAIN("http://localhost:9090"),
 	COMPANY_EMAIL("cxyxj98@gmail.com"),
 	SMTP_AUTHENTICATION_ID("cxyxj98@gmail.com"),
 	SMTP_AUTHENTICATION_PASSWORD("ehdwls1219!"),
--- spring06_toyProject/src/test/java/com/kh/spring/common/MailSenderTest.java
+++ spring06_toyProject/src/test/java/com/kh/spring/common/MailSenderTest.java
@@ -1,6 +1,7 @@
 package com.kh.spring.common;
 
 import java.util.Date;
+import java.util.Map;
 
 import javax.mail.Message;
 import javax.mail.internet.MimeMessage;
@@ -13,14 +14,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kh.spring.member.model.dto.Member;
 
 @WebAppConfiguration
@@ -32,6 +40,9 @@ public class MailSenderTest {
  
 	@Autowired
 	RestTemplate http;
+	
+	@Autowired
+	ObjectMapper mapper;
 	
 	Logger logger = LoggerFactory.getLogger(this.getClass());
 	
@@ -47,17 +58,36 @@ public class MailSenderTest {
 	   }
 	   
 	   @Test
-	   public void restTemplateTest() {
-		 //  String naver = http.getForObject("https://www.naver.com", String.class);
-		MultiValueMap<String,String> body = new LinkedMultiValueMap<>();
-		   body.add("userId", "DEV");
-		   body.add("password", "00009999");
-		   HttpHeaders headers = new HttpHeaders();
-		  headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+	   public void restTemplateTest() throws JsonMappingException, JsonProcessingException, RestClientException {
+		RequestEntity<Void> request = RequestEntity
+				.get("https://dapi.kakao.com/v3/search/book?query=java")
+		.header("Authorization","KakaoAK 3b010db0aad70a7ae662023b0b3070f1")
+		.build();
+		
+		JsonNode root = mapper.readTree(http.exchange(request,String.class).getBody());
+		
+		for(JsonNode jsonNode : root.findValues("url")) {
+			logger.debug(jsonNode.asText());
+		}
+		
+	   }
+	   
+	   @Test
+	   public void restTemplatePostTest() throws Exception{ //파파고api 사용 
 		   
-		  HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(body,headers);
-		   String login = http.postForObject("http://localhost:9090/member/login",entity, String.class);
+		   MultiValueMap<String, String> body = new LinkedMultiValueMap<String, String>();
+		   body.add("source", "en");
+		   body.add("target", "ko");
+		   body.add("text", "If set to true, an XmlMapper will be created using its default constructor. This is only applicable to build");
+		   RequestEntity<MultiValueMap<String, String>> request =
+				   RequestEntity.post("https://openapi.naver.com/v1/papago/n2mt")
+				   .header("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+				   .header("X-Naver-Client-Id", "2Cpov3oDxeRLxX9nLL99")
+				   .header("X-Naver-Client-Secret", "VlwT5t5SzV")
+				   .body(body);
 		   
-		   //logger.debug(naver);
+		   JsonNode root = mapper.readTree(http.exchange(request,String.class).getBody());
+			
+			logger.debug(root.findValue("translatedText").asText());
 	   }
 }
